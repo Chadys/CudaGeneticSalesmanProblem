9e9f6ce7a237023c915e6d4e53c3bedcae065435
chadys <chadys@hotmail.fr>
2018-11-11 01:20:35 +0100

    migration

--- solver.cu
+++ solver.cu
@@ -37,6 +37,13 @@ __device__ void random_init(Individu *individu, curandState_t *state){
     }
 }
 
+__device__ Individu select_migrant(Individu *migrants, curandState_t *state) {
+    unsigned short index = (unsigned short)(curand_uniform(state) * N_ISLAND);
+    if (index == blockIdx.x)
+        index = (unsigned short)((index + 1) % N_ISLAND);
+    return migrants[index];
+}
+
 __device__ void select_mutation(curandState_t *state, unsigned short *mutation) {
     mutation[0] = (unsigned short)(curand_uniform(state) * N_CITIES);
     mutation[1] = (unsigned short)(curand_uniform(state) * N_CITIES);
@@ -59,9 +66,10 @@ __device__ void select_parents(curandState_t *state, int *parents, int numbersOf
 __device__ void mix_parents(Individu *population, curandState_t *state, int replacedIndex, int *parents,
                             int numbersOfParents) {
     int chunkSize = ceil((float)N_CITIES / numbersOfParents);
+    int taken;
     for (int citiesCount = 0 ; citiesCount < N_CITIES ; citiesCount += taken) {
         int selected_parent = parents[curand(state) % numbersOfParents];//(chunkSize * 2)
-        int taken = curand(state) % (chunkSize * 2);
+        taken = curand(state) % (chunkSize * 2);
         if(citiesCount + taken > N_CITIES)
             taken = N_CITIES - citiesCount; // si on dépasse, on prend le reste
         for(int i = citiesCount; i < citiesCount + taken; ++i) {
@@ -95,10 +103,14 @@ __global__ void solve(Individu *migrants) {
 
     random_init(population + threadIdx.x, &state);
     update_score(population + threadIdx.x);
+
+    if (threadIdx.x == 0) {
+        //fill this block's migrant as soon as possible to be sure first migrant selection from another island won't get an uninitialized individual
+        migrants[blockIdx.x] = population[0];
+    }
     __syncthreads();
     bubble_sort(population);
     //TODO replace with better sort
-    __syncthreads();
 
 
     // Main generation loop
@@ -106,31 +118,28 @@ __global__ void solve(Individu *migrants) {
         __syncthreads();
         if (threadIdx.x == 0) {
             printf("GENERATION %d\n", i);
-//            migrants[blockIdx.x] = population[blockDim.x-1];
-        }
-
-        if(is_gonna_die(&state)) {
+            migrants[blockIdx.x] = population[blockDim.x-1]; //export migrant
+            population[0] = select_migrant(migrants, &state); //import migrant
+        } else if(is_gonna_die(&state)) {
             int parents[3];
-            select_parents(population, &state, parents, 3);
+            select_parents(&state, parents, 3);
             printf("%d is dying. New parents : %d & %d & %d\n", threadIdx.x, parents[0], parents[1], parents[2]);
 //            print_path(population[parents[0]]);
 //            print_path(population[parents[1]]);
 //            print_path(population[parents[2]]);
             mix_parents(population, &state, threadIdx.x, parents, 3);
+            update_score(&population[threadIdx.x]);
 //            print_path(population[threadIdx.x]);
         } else if(is_mutating(&state)) {
             printf("%d is mutating.\n", threadIdx.x);
             unsigned short citiesToBeExchanged[2];
             select_mutation(&state, citiesToBeExchanged);
             swap_cities(population + threadIdx.x, citiesToBeExchanged);
+            update_score(&population[threadIdx.x]);
         }
-        /*
-        //TODO migration
-        update_score(&population[threadIdx.x]);
+
         __syncthreads();
         bubble_sort(population);
-        __syncthreads();
-         */
     }
 
 }
