3b2a3cc1082e735cc00af232e14c670668457ff0
chadys <chadys@hotmail.fr>
2018-12-05 00:01:41 +0100

    image file informative title

--- create_path_graph.py
+++ create_path_graph.py
@@ -5,16 +5,19 @@ with open('/tmp/Output.json', 'r') as file:
     graph_object = json.loads(file.read())
 
 for index, island in enumerate(graph_object['islands']):
+    fig = plt.figure()
     plt.title('Result for {} cities, {} islands and {} generations ; island {}'.format(len(graph_object['cities']),
                                                                                        len(graph_object['islands']),
                                                                                        graph_object['nGeneration'],
                                                                                        index))
-    fig = plt.figure()
     xdata = []
     ydata = []
     for island_index in island:
         xdata.append(graph_object['cities'][island_index][0])
         ydata.append(graph_object['cities'][island_index][1])
     plt.plot(xdata, ydata)
-    fig.savefig('graph'+str(index)+'.png')
+    fig.savefig('graph_ncities{}_nislands{}_ngeneration{}_island{}.png'.format(len(graph_object['cities']),
+                                                                               len(graph_object['islands']),
+                                                                               graph_object['nGeneration'],
+                                                                               index))
     plt.close(fig)
